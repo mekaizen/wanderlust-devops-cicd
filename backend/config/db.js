3ea9b3f05e4657f8585eb8168e9bcf3498094e41
mekaizen <nirupamghosh.blrin@gmail.com>
2024-10-25 10:01:20 +0530

    update mongo file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,40 @@
-import mongoose from 'mongoose';
-import { MONGODB_URI } from './utils.js';
-export default function connectDB() {
-  try {
-    mongoose.connect(MONGODB_URI);
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
+//import mongoose from 'mongoose';
+//import { MONGODB_URI } from './utils.js';
+//export default function connectDB() {
+//  try {
+//    mongoose.connect(MONGODB_URI);
+//  } catch (err) {
+//    console.error(err.message);
+//    process.exit(1);
+//  }
+//
+//  const dbConnection = mongoose.connection;
+//
+//  dbConnection.once('open', () => {
+//    console.log(`Database connected: ${MONGODB_URI}`);
+//  });
+//
+//  dbConnection.on('error', (err) => {
+//    console.error(`connection error: ${MONGODB_URI}`);
+//  });
+//  return;
+//}
 
-  const dbConnection = mongoose.connection;
 
-  dbConnection.once('open', () => {
-    console.log(`Database connected: ${MONGODB_URI}`);
-  });
+const connectDB = async () => {
+    if (process.env.SKIP_MONGO === 'true') {
+        console.log('Skipping MongoDB connection due to SKIP_MONGO flag.');
+        return; // Skip MongoDB connection if the flag is set
+    }
 
-  dbConnection.on('error', (err) => {
-    console.error(`connection error: ${MONGODB_URI}`);
-  });
-  return;
-}
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1); // Exit process with failure
+    }
+};
